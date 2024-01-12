c6114fa181bb2ed35b6d01a0bb5a785a1bb53580
Rohitkumar367 <rohitddroid@gmail.com>
2024-01-12 10:22:13 +0530

    Started Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import CartItem from '../components/CartItem'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
+
+  const {cart} = useSelector((state) => state);
+
   return (
     <div>
+      {
+        cart.length > 0 ?
+        (
+          <div>
+            {
+              cart.map((curElem, index) => {
+                return (
+                  <CartItem key={curElem.id} item={curElem} itemIndex={index}></CartItem>
+                )
+              })
+            }
+          </div>
+        )
+         : 
+        (
+          <div>
+            <h1>Cart Empty</h1>
+            <Link to="/">
+              <button>
+                Shop Now
+              </button>
+            </Link>
+          </div>
+        )
+      }
       
     </div>
   )

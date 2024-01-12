621d5479b22a8e586a47e3e9f53d8c1840e25e3b
Rohitkumar367 <rohitddroid@gmail.com>
2024-01-12 10:48:05 +0530

    Done with Cart and CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {FcDeleteDatabase} from 'react-icons/fc'
 
-const CartItem = () => {
+const CartItem = ({item, itemIndex}) => {
   return (
     <div>
       
+      <div>
+
+        <div>
+          <img src={item.image} />
+        </div>
+        <div>
+          <h1>{item.title}</h1>
+          <h1>{item.description}</h1>
+          <div>
+            <p>{item.price}</p>
+            <div>
+              <FcDeleteDatabase></FcDeleteDatabase>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import CartItem from '../components/CartItem'
 import { Link } from 'react-router-dom'
@@ -6,6 +6,13 @@ import { Link } from 'react-router-dom'
 const Cart = () => {
 
   const {cart} = useSelector((state) => state);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    setTotalAmount(
+      cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0)
+    )
+  },[cart])
 
   return (
     <div>
@@ -13,13 +20,36 @@ const Cart = () => {
         cart.length > 0 ?
         (
           <div>
-            {
-              cart.map((curElem, index) => {
-                return (
-                  <CartItem key={curElem.id} item={curElem} itemIndex={index}></CartItem>
-                )
-              })
-            }
+
+            <div>
+              {
+                cart.map((curElem, index) => {
+                  return (
+                    <CartItem key={curElem.id} item={curElem} itemIndex={index}></CartItem>
+                  )
+                })
+              }
+            </div>
+
+            <div>
+
+              <div>
+                <div>Your Cart</div>
+                <div>Summary</div>
+                <p>
+                  <span>Total Items: {cart.length}</span>
+                </p>
+              </div>
+
+              <div>
+                <p>Total Amount: {totalAmount}</p>
+                <button>
+                  Checkout Now
+                </button>
+              </div>
+
+            </div>
+
           </div>
         )
          : 
